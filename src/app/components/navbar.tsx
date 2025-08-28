373c346dd6492c2ba063f1963a505394d48db626
Kcodes09 <keshavgiridharan09@gmail.com>
2025-08-29 00:54:20 +0530

    update

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="fixed top-0 left-0 w-full bg-gray-800 shadow-md z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
-          {/* Logo */}
+          
           <Link href="/" className="flex items-center gap-2">
             <img src="/Spectrum.svg" alt="Spectrum" className="h-8 w-auto" />
             <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 via-yellow-600 to-pink-500 bg-clip-text text-transparent hover:bg-gradient-to-l sm:text-xl">
@@ -15,11 +15,11 @@ function Navbar() {
             </span>
           </Link>
 
-          {/* Links */}
-          <div className="flex space-x-4">
+
+          <div className="flex space-x-1 lg:space-x-4 md:space-x-2">
             <Link
               href="/"
-              className="flex items-center gap-2 sm:gap-1 sm:px-1 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
+              className="flex items-center gap-2 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
             >
               <HomeIcon className="h-5 w-5" />
               <span className="hidden md:inline">Home</span>
@@ -27,7 +27,7 @@ function Navbar() {
 
             <Link
               href="#articles"
-              className="flex items-center gap-2 sm:gap-1 sm:px-1 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
+              className="flex items-center gap-2 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
             >
               <ClipboardPenIcon className="h-5 w-5" />
               <span className="hidden md:inline">Articles</span>
@@ -35,7 +35,7 @@ function Navbar() {
 
             <Link
               href="#profs"
-              className="flex items-center gap-2 sm:gap-1 sm:px-1 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
+              className="flex items-center gap-2 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
             >
               <MicIcon className="h-5 w-5" />
               <span className="hidden md:inline">Talk With Professors</span>
@@ -43,7 +43,7 @@ function Navbar() {
 
             <Link
               href="#aboutus"
-              className="flex items-center gap-2 sm:gap-1 sm:px-1 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
+              className="flex items-center gap-2 text-gray-400 hover:text-white hover:bg-blue-600 px-3 py-1 rounded-md transition"
             >
               <InfoIcon className="h-5 w-5" />
               <span className="hidden md:inline">About Us</span>
@@ -56,4 +56,5 @@ function Navbar() {
 }
 
 export default Navbar;
+
 
